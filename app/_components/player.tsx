cf1349ed5f9b141a0737714ec791868fa5c2692b
Eduardo <eduardo.moraes@clicksoft.com.br>
2024-02-25 06:12:04 -0300

    feat: add fixed bottom music player

--- app/_components/player.tsx
+++ app/_components/player.tsx
@@ -4,7 +4,9 @@ import AudioPlayer from "react-h5-audio-player";
 import "react-h5-audio-player/lib/styles.css";
 import {
   PauseCircle,
+  PauseIcon,
   PlayCircle,
+  PlayIcon,
   SkipBack,
   SkipForward,
 } from "lucide-react";
@@ -16,23 +18,25 @@ const MusicPlayer = () => {
 
   return (
     currentSongUrl && (
-      <div className="w-screen fixed bottom-0 left-0 z-50">
+      <div className="w-screen fixed bottom-0 left-0 z-50 flex items-center justify-center bg-black py-2">
         <AudioPlayer
           autoPlay
           src={currentSongUrl}
+          layout="stacked-reverse"
           onPlay={(e) => setIsPlaying(true)}
           onPause={(e) => setIsPlaying(false)}
           customAdditionalControls={[]}
           customVolumeControls={[]}
           showSkipControls={true}
           showDownloadProgress={false}
+          className="w-[90%] "
           showJumpControls={false}
           ref={playerRef}
           customIcons={{
-            play: <PlayCircle size={30} className="text-primaryOrange" />,
-            pause: <PauseCircle size={30} className="text-primaryOrange" />,
-            previous: <SkipBack size={30} className="text-primaryOrange" />,
-            next: <SkipForward size={30} className="text-primaryOrange" />,
+            play: <PlayIcon size={30} className="text-white fill-white" />,
+            pause: <PauseIcon size={30} className="text-white fill-white" />,
+            previous: <SkipBack size={25} className="text-white" />,
+            next: <SkipForward size={25} className="text-white" />,
           }}
 
           // other props here
